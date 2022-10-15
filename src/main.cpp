31f98eebdff708512b74aef986c220598f675ea6
Patrick Shriwise <pshriwise@gmail.com>
2022-10-15 00:30:15 -0500

    Adding arg parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 
+#include <iostream>
+#include <string>
 
-int main() {
 
+#include "argparse/argparse.hpp"
+
+int main(int argc, char** argv) {
+
+    argparse::ArgumentParser args("GPRT H5M READER");
+
+    args.add_argument("filename");
+
+    try {
+    args.parse_args(argc, argv);                  // Example: ./main -abc 1.95 2.47
+    }
+    catch (const std::runtime_error& err) {
+    std::cout << err.what() << std::endl;
+    std::cout << args;
+    exit(0);
+    }
+
+    auto filename = args.get<std::string>("filename");
+
+    std::cout << "Filename is: " << filename << std::endl;
 
     return 0;
 }
